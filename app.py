0e3a671c3d31d54575b2bcf6eac7f8ffb222ffdd
MehakKambo <mehakpreetp12@gmail.com>
2023-09-12 16:21:01 -0700

    Setup database connection

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import os
+import psycopg2
+from dotenv import load_dotenv, find_dotenv
+from markupsafe import escape
 
 app = Flask(__name__)
+load_dotenv(find_dotenv())
+app.config['CONNECTION_STRING'] = os.environ.get('CONNECTION_STRING')
+
 
 @app.route("/hi")
 def hello():
