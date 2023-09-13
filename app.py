0e94ddb24e48e661bff3bcf41a698a867c0f97ce
MehakKambo <mehakpreetp12@gmail.com>
2023-09-13 12:28:30 -0700

    Added radome quote endpoint

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, Response
 import os
 import psycopg2
-from dotenv import load_dotenv, find_dotenv
-from markupsafe import escape
+import json
 
 app = Flask(__name__)
-load_dotenv(find_dotenv())
 app.config['CONNECTION_STRING'] = os.environ.get('CONNECTION_STRING')
 
-
-@app.route("/hi")
-def hello():
-    return "<p>Hello, World!</p>"
+# The default route returns a random quote
+@app.route("/", methods=['GET'])
+def get_random_quote():
+    conn = psycopg2.connect(app.config['CONNECTION_STRING'])
+    query = """
+    SELECT Quotes.text, Authors.name, Categories.name
+    FROM Quotes
+    JOIN Authors ON Quotes.authorID = Authors.ID
+    JOIN Categories ON Quotes.categoryID = Categories.ID
+    ORDER BY random()
+    LIMIT 1;
+    """
+    cursor = conn.cursor()
+    cursor.execute(query)
+    quote, author_name, category = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    
+    response_data = {'quote': quote, 'author': author_name, 'category': category}
+    json_response = json.dumps(response_data)
+    
+    return Response(json_response, content_type='application/json')
